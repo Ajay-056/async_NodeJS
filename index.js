316292b39c231175a3e26e565f871ce90c2bf13c
AK <balaajay19@gmail.com>
2020-09-07 23:07:32 +0530

    Writed random url in text file

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
       console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Random dog image save to file!');
+      });
     });
 });

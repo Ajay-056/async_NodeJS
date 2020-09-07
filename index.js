885e0124d9368d6096b7f54c0161d4a50670bf48
AK <balaajay19@gmail.com>
2020-09-07 23:01:57 +0530

    Added dog API

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const fs = require("fs");
+const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body.message);
+    });
 });
